f148c5930d91c513cad4d6c1a2183cacd4037802
InisibleCatA1 <shieldscoen@gmail.com>
2023-04-17 22:50:39 -0600

    That fucking setup shit lol, 3rd time rewriting this

--- src/main/java/dev/totaltax/particle/Particle.java
+++ src/main/java/dev/totaltax/particle/Particle.java
@@ -1,10 +1,10 @@
 package dev.totaltax.particle;
 
+import dev.totaltax.particle.command.CommandManager;
 import dev.totaltax.particle.event.EventManager;
 import dev.totaltax.particle.event.EventTarget;
 import dev.totaltax.particle.event.impl.EventKey;
 import dev.totaltax.particle.module.ModuleManager;
-import io.netty.util.internal.logging.InternalLogger;
 import net.minecraft.client.Minecraft;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,18 +14,22 @@ public class Particle {
     private String version = "0.0.1-dev";
     private Logger logger = LogManager.getLogger("Particle");
     private static Particle instance = new Particle();
-    private ModuleManager moduleManager;
 
     // Import stuff...
+    private ModuleManager moduleManager;
+    private CommandManager commandManager;
     private EventManager eventManager;
 
 
     public void start() {
         logger.info("Initializing Particle...");
+        Minecraft.getInstance().getWindow().setTitle(name + " >> " + version);
         eventManager = new EventManager();
         moduleManager = new ModuleManager();
+        commandManager = new CommandManager();
 
         moduleManager.init();
+        commandManager.init();
 
         logger.info("Done!");
         eventManager.register(this);
@@ -42,6 +46,25 @@ public class Particle {
     public EventManager getEventManager() {
         return eventManager;
     }
+    public ModuleManager getModuleManager() {
+        return moduleManager;
+    }
+
+    public CommandManager getCommandManager() {
+        return commandManager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
 
     @EventTarget
     public void onKey(EventKey event) {
@@ -50,9 +73,5 @@ public class Particle {
                 m.toggle();
             }
         });
-    }
-
-    public Logger getLogger() {
-        return logger;
     }
 }
--- src/main/java/dev/totaltax/particle/command/CommandManager.java
+++ src/main/java/dev/totaltax/particle/command/CommandManager.java
@@ -1,4 +1,22 @@
 package dev.totaltax.particle.command;
 
+import org.reflections.Reflections;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class CommandManager {
+    private List<Command> commands = new CopyOnWriteArrayList<>();
+
+    public void init() {
+        new Reflections("dev.totaltax.particle.command.impl").getSubTypesOf(Command.class).forEach(m -> {
+            try {
+                commands.add(m.newInstance());
+            } catch (InstantiationException | IllegalAccessException ignore) {}
+        });
+    }
+
+    public List<Command> getModules() {
+        return commands;
+    }
 }
